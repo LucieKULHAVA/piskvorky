d048ae12b5369522e7a71054f8cc54441336fbe8
LucieKULHAVA <46672152+LucieKULHAVA@users.noreply.github.com>
2021-04-29 19:45:06 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
+"use strict";
 
+let hraje = 'circle';
+let symbol = document.querySelector('.kolecko');
+/*let ctverecek = document.querySelectorAll(".policko");*/
+
+ const Hra = (event) => {
+   let ctverecek = event.target;
+
+   if (ctverecek.className === 'policko') {
+ 
+     if (hraje === 'circle') {
+      
+       ctverecek.classList.add('board__field--circle');   
+       hraje = 'cross';
+       symbol.src = 'images/cross.svg';
+       ctverecek.disabled = true;
+     }
+     else if (hraje === "cross") {
+       ctverecek.classList.add("board__field--cross");
+       hraje = "circle";
+       symbol.src = "images/circle.svg";
+      ctverecek.disabled = true;
+    
+     }
+    }
+ };
+
+ let klik = document.querySelectorAll(".policko");
+ klik = addEventListener("click", Hra);
